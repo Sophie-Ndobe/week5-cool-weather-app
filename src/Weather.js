76c614d759e54bea454dc0038a36da0d25ebe00d
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-21 21:07:15 +0200

    Updated the date and time

--- src/Weather.js
+++ src/Weather.js
@@ -2,13 +2,39 @@ import React from "react";
 import "./Weather.css";
 
 export default function Weather() {
+  let date = new Date();
+
+  let hours = date.getHours();
+  if (hours < 10) {
+    hours = `0${hours}`;
+  }
+
+  let minutes = date.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+
+  let day = days[date.getDay()];
+
   return (
     <div className="Weather mt-5">
       <div className="row">
         <div className="col-6">
           <h1>San Francisco</h1>
           <ul>
-            <li>Saturday 11:42, few clouds</li>
+            <li>
+              {day} {hours}:{minutes}, few clouds
+            </li>
             <li>
               Humidity: <strong>86%</strong>, Wind: <strong>2.68km/h</strong>{" "}
             </li>
