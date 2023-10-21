2f754452f1465ecc271abe8b8183722a2c6afeba
Sophie-Ndobe <ndobes76@gmail.com>
2023-10-21 14:55:18 +0200

    Created a form with a search engine and added css styling

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 import "./App.css";
+import Search from "./Search";
 import Footer from "./Footer";
 
 function App() {
   return (
     <div className="App">
       <div className="Container">
-        <h1>Weather App</h1>
-        <button className="btn btn-primary">Search</button>
+        <Search />
         <Footer />
       </div>
     </div>
